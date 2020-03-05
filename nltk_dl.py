733840ba8fac761bc6f73ce4a69ebe913b8a57e1
Zhili <zhilijwang@gmail.com>
2020-03-05 00:03:59 -0600

    eda and readme

---
+++ nltk_dl.py
@@ -0,0 +1,6 @@
+import nltk
+# get 'stopwords' and 'reuters' following the navigation
+nltk.download('stopwords')
+nltk.download('reuters')
+# or run these if it crashes your system:
+nltk.download_shell()
